6409d6d1171a98bdb6b92d3dc2cda46b5265cd06
PragatiDahal <pragatidahal2058@gmail.com>
2024-08-19 20:54:11 +0545

    update

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -50,7 +50,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/resume1.pdf",
+      href: "/resume-.pdf",
       style: "rounded-br-md",
       download: true,
     },
